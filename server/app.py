bab9f2a60ec1ce9fc8de3af1e17b14a24a2a73f7
Mark <ajineer@outlook.com>
2023-08-25 13:55:02 -0600

    created models and did some migrations to set up database, set up some basic views for signinup/sign in, fixed some imports in the pipfile

--- server/app.py
+++ server/app.py
@@ -3,12 +3,13 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, session
 from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
 # Add your model imports
+from models import User, Book, Recipe
 
 
 # Views go here!
@@ -16,6 +17,65 @@ from config import app, db, api
 @app.route('/')
 def index():
     return '<h1>Phase 4 Project Server</h1>'
+
+class Signup(Resource):
+
+    def post(self):
+        username = request.get_json().get('username')
+        password = request.get_json().get('password')
+
+        if username and password and not User.query.filter(User.username == username).first():
+            new_user = User(username = username)
+            new_user.password_hash = password
+            db.session.add(new_user)
+            db.session.commit()
+            session['user_id'] = new_user.id
+            return new_user.to_dict(rules=('books',)), 201
+        
+        return {'error': '422 Unprocessable Entity'}, 422
+    
+
+class Login(Resource):
+
+    def post(self):
+
+        data = request.get_json()
+        username = data['username']
+        password = data['password']
+
+        user = User.query.filter(User.username == username).first()
+
+        if user:
+            if user.authenticate(password):
+                session['user_id'] = user.id
+                return user.to_dict(), 200
+        
+        return {'error': "Unauthorized"}, 401
+
+class CheckSession(Resource):
+
+    def get(self):
+        if session.get('user_id'):
+            user = User.query.filter(User.id == session.get('user_id')).first()
+            return user.to_dict(), 200
+        return {'error': 'Unauthorized'}, 401
+
+class Logout(Resource):
+
+    def delete(self):
+
+        if session.get('user_id'):
+            session['user_id'] = None
+            return {}, 204
+        
+        return {'error': 'Unauthorized'}, 401
+    
+
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
+api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+
 
 
 if __name__ == '__main__':
