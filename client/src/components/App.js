0a9c0862c5f63c59bb1a9d2f7be4d4ddb1860748
Gunnytame <Gunnar.95email@gmail.com>
2023-08-28 11:36:54 -0600

    initial commit

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,4 +5,15 @@ function App() {
   return <h1>Phase 4 Project Client</h1>;
 }
 
+handleSubmit = (event) => {
+  fetch('http://localhost:5555', {
+      method: 'POST',
+            }).then(function(response) {
+      console.log(response)
+      return response.json();
+    });
+
+  event.preventDefault();
+}
+
 export default App;
