d92fb44fbc560919b5ca1e820fa35aaca59513cf
Gunnytame <Gunnar.95email@gmail.com>
2023-08-28 13:30:44 -0600

    monday changes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,19 +1,24 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
+import './FlipBook.css';
 
 function App() {
   return <h1>Phase 4 Project Client</h1>;
 }
 
-handleSubmit = (event) => {
-  fetch('http://localhost:5555', {
-      method: 'POST',
-            }).then(function(response) {
-      console.log(response)
-      return response.json();
-    });
+const FlipBook = () => {
+  const [isFlipped, setIsFlipped] = useState(false);
 
-  event.preventDefault();
-}
+  const handleFlip = () => {
+    setIsFlipped(!isFlipped);
+  };
 
-export default App;
+  return (
+    <div className={`flip-book ${isFlipped ? 'flipped' : ''}`} onClick={handleFlip}>
+      <div className="page page-front">Page 1</div>
+      <div className="page page-back">Page 2</div>
+    </div>
+  );
+};
+
+export default FlipBook;
