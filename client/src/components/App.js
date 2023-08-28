10f5131c4644881825a428a2d7c007f1a70793ad
Gunnytame <Gunnar.95email@gmail.com>
2023-08-28 14:42:08 -0600

    Monday changes 2

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, setIsFlipped } from "react";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
@@ -17,6 +17,6 @@ const FlipBook = () => {
       <div className="page page-back">Page 2</div>
     </div>
   );
-};
+
 
 export default FlipBook;
