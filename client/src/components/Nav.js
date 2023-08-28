097fcdabda308415ff912bfa33eed9804dab8673
Mark <ajineer@outlook.com>
2023-08-28 12:17:06 -0600

    created nav bar component with react router dom

---
+++ client/src/components/Nav.js
@@ -0,0 +1,20 @@
+import { NavLink } from "react-router-dom";
+
+
+function Nav(){
+
+    return (
+        <div>
+            <h1>Navigation</h1>
+            <NavLink to={"/"} exact>
+                <span>Home</span>
+            </NavLink>
+            <NavLink to={'/books'} exact>
+                <span>My Books</span>
+            </NavLink>
+            <NavLink to={'/signup'} exact>
+                <span>Sign up</span>
+            </NavLink>
+        </div>
+    )
+}
