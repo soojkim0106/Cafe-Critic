8e662b9c24f3b7009b5923db532f07840b59d965
Gunnytame <Gunnar.95email@gmail.com>
2023-08-29 14:17:16 -0600

    light css changes

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -1,17 +1,38 @@
-function Carousel(books, prev, current, next, loadBook, prevBook, nextBook){
+import React, { useState } from 'react';
+import './index.css'; // Make sure to import your CSS file
 
-    return (
-        <div>
-            <div>
-                {books[prev] === undefined ? <div></div> : <div><h2>{books[prev].category}</h2></div>}
-                {books[current] === undefined ? <div></div> : <div onClick={loadBook(books[current])}><h2>{books[current].category}</h2></div>}
-                {books[prev] === undefined ? <div></div> : <div><h2>{books[next].category}</h2></div>}
-            </div>
-            <div>
-                <button onClick={prevBook}>{'\u276c'}</button><button onClick={nextBook}>{'\u276d'}</button>
-            </div>
-        </div>
-    )
-}
+const Carousel = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
 
-export default Carousel
+  const images = [
+    'image1.jpg',
+    'image2.jpg',
+    'image3.jpg'
+  ];
+
+  const prevSlide = () => {
+    setActiveIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+  };
+
+  const nextSlide = () => {
+    setActiveIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+  };
+
+  return (
+    <div className="carousel">
+      <div className="carousel-container" style={{ transform: `translateX(-${activeIndex * 100}%)` }}>
+        {images.map((image, index) => (
+          <div key={index} className="carousel-item">
+            <img src={image} alt={`Image ${index + 1}`} />
+          </div>
+        ))}
+      </div>
+      <div className="carousel-nav">
+        <button onClick={prevSlide}>Previous</button>
+        <button onClick={nextSlide}>Next</button>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
