cae6e55d0512af5f035aecf2bd0a5109709c92d4
Sajiya <misa7ee@gmail.com>
2023-10-13 09:06:49 -0400

    models, weather api

--- server/models.py
+++ server/models.py
@@ -1,6 +1,67 @@
-from sqlalchemy_serializer import SerializerMixin
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 from sqlalchemy.ext.associationproxy import association_proxy
-
+from sqlalchemy_serializer import SerializerMixin
 from config import db
 
-# Models go here!
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
+
+# -------------------------USER -------------------------
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    
+    closets = db.relationship('Closet', back_populates='user')
+
+    serialize_rules = ('-closets.user',)
+    
+# -------------------------CLOTHINGITEM-------------------------
+class ClothingItem(db.Model, SerializerMixin):
+    __tablename__ = 'clothingitems'
+ 
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    category = db.Column(db.String, nullable=False)
+    tags = db.Column(db.String, nullable=False)
+    
+    closets = db.relationship('Closet', back_populates='clothingitem')
+    
+    serialize_rules = ('-closets.clothingitem')
+    
+# -------------------------CLOSET (bridge)-------------------------
+class Closet(db.Model, SerializerMixin):
+    __tablename__ = 'closets'
+
+    id= db.Column(db.Integer, primary_key=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    clothingitem_id = db.Column(db.Integer, db.ForeignKey('clothingitems.id'))
+
+    user = db.relationship('User', back_populates='closets')
+    clothingitem = db.relationship('ClothingItem', back_populates='closets')
+    
+    serialize_rules = ('-user.closets', '-clothingitem.closets',)
+
+
+
+# -------------------------WARDROBE-------------------------
+
+#  in the future, i may want to use this as a recommendation engine for users (curated for each client)
+
+
+# class Wardrobe(db.Model, SerializerMixin):
+#     __tablename__ = 'wardrobes'
+    
+#     id = db.Column(db.Integer, primary_key=True, nullable=False)
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+#     user = db.relationship('User', back_populates='wardrobe')
+#     clothing_items = db.relationship('ClothingItem', secondary=wardrobe_clothing_items, back_populates='wardrobes')
+
+        
+
+
--- server/seed.py
+++ server/seed.py
@@ -1,17 +1,57 @@
-#!/usr/bin/env python3
-
-# Standard library imports
-from random import randint, choice as rc
-
-# Remote library imports
+from your_flask_app import db, User, Closet, ClothingItem
 from faker import Faker
 
-# Local imports
-from app import app
-from models import db
+# Initialize Faker for generating fake user data
+fake = Faker()
 
-if __name__ == '__main__':
-    fake = Faker()
+# Function to seed the database 🌱
+def seed_database():
+    # Create and add users 👤
+    users_data = []
+
+    for _ in range(3):
+        user_data = {
+            "username": fake.user_name(),
+            "email": fake.email(),
+            "password": "password1",  # You can set a common password for fake users
+        }
+        users_data.append(user_data)
+
+    for user_data in users_data:
+        user = User(**user_data)
+        db.session.add(user)
+
+    # Create and add closets 🚪
+    closets_data = [
+        {"user_id": 1, "name": "User 1's Closet"},
+        {"user_id": 2, "name": "User 2's Closet"},
+        {"user_id": 3, "name": "User 3's Closet"},
+    ]
+
+    for closet_data in closets_data:
+        closet = Closet(**closet_data)
+        db.session.add(closet)
+
+    # Create and add clothing items 👗👟👒
+    clothing_items_data = [
+        {"name": "Blue Jeans", "description": "Classic blue jeans", "category": "Pants", "closet_id": 1, "color": "Blue", "fabric": "Denim", "size": "32x34"},
+        {"name": "Running Shoes", "description": "Comfortable running shoes", "category": "Shoes", "closet_id": 1, "color": "Gray", "fabric": "Mesh", "size": "9"},
+        {"name": "Sun Hat", "description": "Wide-brimmed sun hat", "category": "Hats", "closet_id": 2, "color": "Beige", "fabric": "Straw", "size": "One size"},
+        # Add more clothing items with color, fabric, and size for different users and closets
+    ]
+
+    for item_data in clothing_items_data:
+        clothing_item = ClothingItem(**item_data)
+        db.session.add(clothing_item)
+
+    # Commit the changes to the database 🚀
+    db.session.commit()
+
+if __name__ == "__main__":
+    # Initialize your Flask app and database 🌐
+    from your_flask_app import create_app
+
+    app = create_app()
     with app.app_context():
-        print("Starting seed...")
-        # Seed code goes here!
+        # Call the seed_database function to populate the database 🌿
+        seed_database()
