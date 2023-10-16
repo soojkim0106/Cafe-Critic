ee7e49cb6eaf49e43bbcc1010c262a1d9244a69b
Yurick <YJakse789@gmail.com>
2023-10-16 10:59:31 -0400

    First set of Models complete

--- server/models.py
+++ server/models.py
@@ -4,3 +4,34 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+    adoptions = db.relationship("Adoption", backref="user")
+
+    serialize_rules = ("-adoptions.user",)
+
+class Pet(db.Model, SerializerMixin):
+    __tablename__ = "pets"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+    adoptions = db.relationship("Adoption", backref="pet")
+
+    serialize_rules = ("-adoptions.pet",)
+
+class Adoption(db.Model, SerializerMixin):
+    __tablename__ = "adoptions"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    pet_id = db.Column(db.Integer, db.ForeignKey("pets.id"))
+
+    serialize_rules = ("-user.adoptions", "-pet.adoptions",)
