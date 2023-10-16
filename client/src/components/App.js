ac0fbee7acc8c528e0b57e409d41dca23c9b483a
Ashley Rivera <ashleyariv@gmail.com>
2023-10-16 11:56:47 -0400

    initialization

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,38 @@
 import React, { useEffect, useState } from "react";
+import io from 'socket.io-client';
 import { Switch, Route } from "react-router-dom";
 
-function App() {
-  return <h1>Project Client</h1>;
+let endPoint = 'http://localhost:5555';
+let socket = io.connect(`${endPoint}`);
+
+const App = () => {
+  const [messages, setMessages] = useState(['Welcome User']);
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    getMessages()
+  }, [messages.length]);
+
+  const getMessages = () => {
+    socket.on('message', msg => {
+      setMessages([...messages, msg]);
+    });
+  };
+
+  const onChange = e => {
+    setMessage(e.target.value);
+  };
+
+  // const onClick = () => {
+  //   if (message !== '') {
+  //     socket
+  //   }
+  // }
+  return (
+    <div>
+      <p>Hello World!</p>
+    </div>
+  )
 }
 
 export default App;
