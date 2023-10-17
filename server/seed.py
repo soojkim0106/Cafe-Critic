8cd20a98f3e6e02cae6e11388d8a1277429331c2
Nial Johnson <nialj707@gmail.com>
2023-10-17 12:36:28 -0700

    Pics in Seed

--- server/seed.py
+++ server/seed.py
@@ -10,13 +10,13 @@ from faker import Faker
 from app import app
 from models import db, User, Pet, Adoption
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     fake = Faker()
     with app.app_context():
         print("Starting seed...")
         # Seed code goes here!
 
-            # This will delete any existing rows
+        # This will delete any existing rows
         # so you can run the seed file multiple times without having duplicate entries in your database
         print("Deleting data...")
         Pet.query.delete()
@@ -25,19 +25,32 @@ if __name__ == '__main__':
         # db.create_all()
 
         print("Creating pets...")
-        pet1 = Pet(name="Bang Kitty", breed='American Shorthair', type='Cat', image="nonUrl")
-        pet2 = Pet(name="Abby", breed= 'Mixed Breed',type='Dog',image="nonUrl")
-        pet3 = Pet(name="Lil King Trashmouth", breed='North American Raccoon', type='Raccoon',image="nonUrl")
-        pet4 = Pet(name="Buddy", breed="Golden Doodle", type="Dog",image="nonUrl")
+        pet1 = Pet(name="Bang Kitty", breed="American Shorthair", type="Cat", image=" ")
+        pet2 = Pet(
+            name="Stuart",
+            breed="Annoyoying",
+            type="Rat",
+            image="https://pbs.twimg.com/media/FPgA0D7XEAAtzr4.jpg",
+        )
+        pet3 = Pet(
+            name="Lil King Trashmouth",
+            breed="North American Raccoon",
+            type="Raccoon",
+            image="https://www.google.com/url?sa=i&url=https%3A%2F%2Fsdzwildlifeexplorers.org%2Fanimals%2Fnorth-american-raccoon&psig=AOvVaw0P2x5pRNInXXEbPBRcQDHJ&ust=1697657134795000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCOi0xuXn_YEDFQAAAAAdAAAAABAd",
+        )
+        pet4 = Pet(
+            name="DOOfus",
+            breed="Wiener",
+            type="Dog",
+            image="https://i.kym-cdn.com/photos/images/masonry/002/304/541/b96.gif",
+        )
         pets = [pet1, pet2, pet3, pet4]
 
         print("Creating users...")
 
         user1 = User(fname="Emma", lname="Firstwright")
-        user2 = User(
-            fname="Geri", lname="Holloway")
-        user3 = User(
-            fname="Melanie", lname="Robles")
+        user2 = User(fname="Geri", lname="Holloway")
+        user3 = User(fname="Melanie", lname="Robles")
         users = [user1, user2, user3]
         db.session.add_all(pets)
         db.session.add_all(users)
@@ -51,4 +64,4 @@ if __name__ == '__main__':
         db.session.add_all(adoptions)
         db.session.commit()
 
-        print("Seeding done!")# Empty tables
+        print("Seeding done!")  # Empty tables
