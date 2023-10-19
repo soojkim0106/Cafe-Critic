613f1de7d1aec6c479a649a0b6839c52f4ac42c3
Yurick <YJakse789@gmail.com>
2023-10-19 13:38:19 -0400

    Made it so you can't favorite pets if not logged in

--- client/src/components/PetCard.js
+++ client/src/components/PetCard.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 function PetCard({ xurl,pet,currUser,postFavorites,removeFavroite }) {
     const { name, breed, image, type, id, favorites } = pet
     const [inFavorite, setInFavorite] = useState(false)
+    const [enableFav, setEnableFav] = useState(false)
 
     function onClick(e){
         if(inFavorite){
@@ -28,24 +29,38 @@ function PetCard({ xurl,pet,currUser,postFavorites,removeFavroite }) {
             if(currUser.favorites.find((searchPet)=>searchPet.pet_id===id)){setInFavorite(true)}
         }
     },[pet])
-    return (
-        <li className="card">
-            <h4>{name}</h4>
-            <img src={image} alt={name} />
-            <p>Type: {type}</p>
-            <p>Breed: {breed}</p>
-            {inFavorite ? (
-                <>
-                    <button onClick={onClick} > Unfavorite </button>
-                    <button >Adopt</button>
-                </>
-            ) : (
-                <>
-                    <button onClick={onClick} className="primary"> Favorite </button>
-                </>
-            )}
-        </li>
-    );
+    useEffect(()=>{
+        if(currUser === ""){setEnableFav(false)}else{setEnableFav(true)}
+    },[currUser])
+    if(enableFav){
+        return (
+            <li className="card">
+                <h4>{name}</h4>
+                <img src={image} alt={name} />
+                <p>Type: {type}</p>
+                <p>Breed: {breed}</p>
+                {inFavorite ? (
+                    <>
+                        <button onClick={onClick} > Unfavorite </button>
+                        <button >Adopt</button>
+                    </>
+                ) : (
+                    <>
+                        <button onClick={onClick} className="primary"> Favorite </button>
+                    </>
+                )}
+            </li>
+        );
+    }else{
+        return (
+            <li className="card">
+                <h4>{name}</h4>
+                <img src={image} alt={name} />
+                <p>Type: {type}</p>
+                <p>Breed: {breed}</p>
+            </li>
+        )
+    }
 }
 
 export default PetCard;
