5d345e529c66aa4d92dc9cc0a3c607cad7f78ec8
Yurick <YJakse789@gmail.com>
2023-10-19 11:33:55 -0400

    Added search bar feature to pets list

--- client/src/components/PetPage.js
+++ client/src/components/PetPage.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import PetList from "./PetList";
 import { NavLink } from "react-router-dom";
-// import Search from "./Search";
+import Search from "./Search";
 
 
 function PetPage({xurl,pets,currUser,postFavorites,removeFavroite}) {
+    const [petsSearch,setPetsSearch] = useState(pets)
+    const [searchTerm,setSearchTerm] = useState("")
 
+    function onSearchTermChange(term){
+        setSearchTerm(term)
+    }
 
+    useEffect(()=>{
+        setSearchFunction()
+    },[pets,searchTerm])
+
+    function setSearchFunction(){
+        setPetsSearch(pets.filter((pet)=>{
+            return pet.name.toLowerCase().includes(searchTerm.toLowerCase()) || pet.type.toLowerCase().includes(searchTerm.toLowerCase())
+        }))
+    }
     return (
         <main>
-            {/* <Search /> */}
+            <Search onSearchTermChange={onSearchTermChange} searchTerm={searchTerm}/>
             <PetList
-                pets={pets} currUser={currUser} xurl={xurl} postFavorites={postFavorites} removeFavroite={removeFavroite}/>
+                pets={petsSearch} currUser={currUser} xurl={xurl} postFavorites={postFavorites} removeFavroite={removeFavroite}/>
         </main>
     )
 }
