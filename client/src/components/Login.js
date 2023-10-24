b601479ade299778b236530602bc208d64795088
Spencer <eklund.spencer@outlook.com>
2023-10-24 16:51:23 -0600

    login form created, needs functionality

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,75 @@
 import { useState } from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 const Login = () => {
+	const [isLogin, setIsLogin] = useState(true);
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [newUsername, setNewUsername] = useState('');
+	const [newPassword, setNewPassword] = useState('');
+	const [confirmPassword, setConfirmPassword] = useState('');
+
+	const toggleForm = () => {
+		setIsLogin(!isLogin);
+	};
+
 	return (
 		<div>
-			<h1>Login</h1>
+			<h2>{isLogin ? 'Login' : 'Create Account'}</h2>
+			<form>
+				{isLogin ? (
+					<>
+						<TextField
+							label="Username"
+							value={username}
+							onChange={(e) => setUsername(e.target.value)}
+							fullWidth
+							required
+						/>
+						<TextField
+							label="Password"
+							value={password}
+							type="password"
+							onChange={(e) => setPassword(e.target.value)}
+							fullWidth
+							required
+						/>
+					</>
+				) : (
+					<>
+						<TextField
+							label="Create Username"
+							value={newUsername}
+							onChange={(e) => setNewUsername(e.target.value)}
+							fullWidth
+							required
+						/>
+						<TextField
+							label="Create Password"
+							value={newPassword}
+							type="password"
+							onChange={(e) => setNewPassword(e.target.value)}
+							fullWidth
+							required
+						/>
+						<TextField
+							label="Confirm Password"
+							value={confirmPassword}
+							type="password"
+							onChange={(e) => setConfirmPassword(e.target.value)}
+							fullWidth
+							required
+						/>
+					</>
+				)}
+				<Button type="submit" variant="contained" color="primary">
+					{isLogin ? 'Login' : 'Create Account'}
+				</Button>
+			</form>
+			<Button onClick={toggleForm}>
+				{isLogin ? 'Create Account' : 'Have an account? Login!'}
+			</Button>
 		</div>
 	);
 };
