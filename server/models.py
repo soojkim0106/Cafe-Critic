5b72061f07f6267354276b42c16d5a31cff6cda4
charlesfeatherstone <charlesfstone11@gmail.com>
2023-11-27 12:32:58 -0500

    added user class and gym class

--- server/models.py
+++ server/models.py
@@ -4,3 +4,16 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+class User(db.Model,SerializerMixin):
+    __tablename__='users'
+
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String)
+    membership_type=db.Column(db.String)
+
+class Gym_location(db.Model,SerializerMixin):
+    __tablename__='gym locations'
+
+    id=db.Column(db.Integer,primary_key=True)
+    Location=db.Column(db.String)
+    number_of_machines=db.Column(db.Integer)
