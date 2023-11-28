03d8de416efdbbdf5ab20f4d595a9e912f05dff5
Nicole Casteel <thepronicole888@gmail.com>
2023-11-28 10:29:31 -0600

    added charles code and nicole code togother. Added Nicoles seeding... may need to be edited.

--- server/models.py
+++ server/models.py
@@ -10,10 +10,81 @@ class User(db.Model,SerializerMixin):
     id=db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String)
     membership_type=db.Column(db.String)
+    goal=db.Column(db.String)
+    #add relationships
+    assignments=db.relationship('Assignment', back_populates='user')
+    exercise=association_proxy('assignments','exercise')
+    reviews=db.relationship('Reviews', back_populates='users', cascade='all, delete-orphan')
+    reviews_gymLo=association_proxy('gym_locations', 'reviews')
 
-class Gym_location(db.Model,SerializerMixin):
-    __tablename__='gym locations'
+class GymLocation(db.Model,SerializerMixin):
+    __tablename__='gym_locations'
+
+    id=db.Column(db.Integer, primary_key=True)
+    Location=db.Column(db.String, nullable=False)
+    number_of_machines=db.Column(db.Integer)
+    #relationships a gym location has many reviews
+    reviews=db.relationship('Reviews', back_populates='gym_locations', cascade='all, delete-orphan')
+    reviews_user=association_proxy('user', 'reviews')
+
+    machines=db.relationship('Machines', backpopulates='gym_location')
+    trainers=db.relationship('Trainers', backpopulates='gym_location')
+
+class Exercise(db.Model,SerializerMixin):
+    __tablename__='exercises'
 
     id=db.Column(db.Integer,primary_key=True)
-    Location=db.Column(db.String)
-    number_of_machines=db.Column(db.Integer)
+    exercise_type=db.Column(db.String) #example:cardio
+    exercise=db.Column(db.String)#example:Pushups
+    #add relationships
+    assignments=db.relationship('Assignment', back_populates='exercise')
+    user=association_proxy('assignments','user')
+
+#assigns exercises to user
+class Assignment(db.Model,SerializerMixin):
+    __tablename__='assignments'
+
+    #assignmenmts will be created by a form using post
+    id=db.Column(db.Integer,primary_key=True)
+    user_id=db.Column(db.Integer,db.ForeignKey('users.id'))
+    user_goal=db.Column(db.String)
+    exercise_id=db.Column(db.Integer,db.ForeignKey('exercises.id'))
+    #add relationships
+    user=db.relationship('User',back_populates='assignments')
+    exercise=db.relationship('Exercise',back_populates='assignments')
+
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+
+    id=db.Column(db.Integer, primary_key=True)
+    body=db.Column(db.String)
+    created_at=db.Column(db.DateTime, server_default=db.func.now())
+    updated_at=db.Column(db.DateTime, onupdate=db.func.now())
+
+    user_id=db.Column(db.Integer, db.ForgeinKey('users.id'))
+    gym_locations=db.Column(db.Integer, db.ForgeinKey('gym_locations.id'))
+    # relationships
+    user=db.relationship('User', back_populates='reviews')
+    gym_location=db.relationship('GymLocation', back_populates='reviews')
+
+class Machines(db.Model, SerializerMixin):
+    __tablename__= 'machines'
+    id=db.Column(db.Integer)
+    machine_type=db.Column(db.String)
+
+    gym_locations=db.Column(db.Integer, db.ForgeinKey('gym_locations.id'))
+
+    #relationship A Gym location has many machines
+    gym_location=db.relationships('GymLocation', backpopulates='machines') 
+
+
+class Trainer(db.Model, SerializerMixin):
+    __tablename__= 'trainers'
+    id=db.Column(db.Integer)
+    names=db.Column(db.String)
+
+    gym_locations=db.Column(db.Integer, db.ForgeinKey('gym_locations.id'))
+
+    #relationship A Gym location has many trainers
+    gym_location=db.relationships('GymLocation', backpopulates='trainers') 
--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,77 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, User, GymLocation, Exercise, Assignment, Review, Machines, Trainer
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
+        print("Deleting date...")
+        User.query.delete()
+        GymLocation.query.delete()
+        Exercise.query.delete()
+        Assignment.query.delete()
+        Review.query.delete()
+        Trainer.query.delete()
+        Machines.query.delete()
+
         print("Starting seed...")
         # Seed code goes here!
+        print('Making users')
+        u1=User(name=fake.name(),membership_type='starter',goal='get big')
+        u2=User(name=fake.name(),membership_type='starter',goal='get big')
+        u3=User(name=fake.name(),membership_type='Pro',goal='get lean')
+        u4=User(name=fake.name(),membership_type='Pro',goal='Tone up')
+        users=[u1,u2,u3,u4]
+
+        print('Making Gym Locations')
+
+        g1=GymLocation(location=fake.address(),number_of_machines=12)
+        g2=GymLocation(location=fake.address(),number_of_machines=12)
+        g3=GymLocation(location=fake.address(),number_of_machines=12)
+        g4=GymLocation(location=fake.address(),number_of_machines=12)
+        gyms=[g1,g2,g3,g4]
+
+        print('Making exercises')
+        
+        e1=Exercise(exercise_type="Cardio",exercise='Running')
+        e2=Exercise(exercise_type="Cardio",exercise='Jump Rope')
+        e3=Exercise(exercise_type="Strength",exercise='Weight lifting')
+        e4=Exercise(exercise_type="Power",exercise='delayed lifts')
+        exer=[e1,e2,e3,e4]
+
+        print('making assignments')
+
+        a1=Assignment(user_id=1,user_goal='get big',exercise_id=3)
+        a2=Assignment(user_id=2,user_goal='get big',exercise_id=4)
+        a3=Assignment(user_id=3,user_goal='get lean',exercise_id=2)
+        a4=Assignment(user_id=4,user_goal='tone up',exercise_id=1)
+        assignments=[a1,a2,a3,a4]
+
+        print('making reviews')
+        r1=Review(user_id=1,body='Very nice gym!',created_at=fake.date(),updated_at=None)
+        r2=Review(user_id=2,body='Staff is awesome!',created_at=fake.date(),updated_at=fake.date())
+        r3=Review(user_id=3,body='Its eh',created_at=fake.date(),updated_at=None)
+        r4=Review(user_id=4,body='4 stars',created_at=fake.date(),updated_at=None)
+        reviews=[r1,r2,r3,r4]
+
+        print('printing machines')
+        m1=Machines(machine_type='Free weights, Sqaut racks, 5 cable towers, cardio, tires, any many more')
+        m2=Machines(machine_type='Free weights, green turf, many squat racks, many bench press any many more')
+        machines=[m1, m2]
+
+        print('printing trainers')
+        t1=Trainer(name=fake.name()[1,10])
+        trainer=[t1]
+        
+
+        db.session.add_all(assignments)
+        db.session.add_all(exer)
+        db.session.add_all(gyms)
+        db.session.add_all(users)
+        db.session.add_all(reviews)
+        db.session.add_all(machines)
+        db.session.commit()
+
+
+        print('seeding done!')
