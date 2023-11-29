a3933a20337f1a279af89c1f6c8646863d6f9cde
charlesfeatherstone <charlesfstone11@gmail.com>
2023-11-29 09:11:11 -0500

    started-frontend

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,17 @@ import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
-  return <h1>Project Client</h1>;
+  return (
+  <div className="homepage-container">
+    <div className="title">The Gym</div>
+    <hr class="solid"></hr>
+    <div className="content">
+      <h1 className="homepage-reviews">Reviews</h1>
+      <h1 className="homepage-checkin">Check in</h1>
+      <h1 className="homepage-workout">Workout Schedule</h1>
+    </div>
+  </div>
+  )
 }
 
 export default App;
