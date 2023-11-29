b0f88092b5d402723c4ff0c66e63c53cfc62d9a6
Emily Valdez <emily.milan.valdez@gmail.com>
2023-11-29 16:34:35 -0600

    font styling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,13 +5,13 @@ import CheckIn from "./CheckIn";
 function Header(){
     return (
         <header>
-            <h1>
+            <h1 style={{fontSize: 50}}>
             The Gym
             </h1>
             <CheckIn></CheckIn>
-            <h3>Sign In</h3>
-            <h3>Register / Sign Up</h3>
-            <h3>Sign Out</h3>
+            <h3 style={{fontSize: 17}}>Sign In</h3>
+            <h3 style={{fontSize: 17}}>Register / Sign Up</h3>
+            <h3 style={{fontSize: 17}}>Sign Out</h3>
         </header>
     )
 }
