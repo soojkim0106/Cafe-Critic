57e84552d391dc63b46e318a3f6de54c17867bfd
Brian Richie <brianrichiesr@Brians-MacBook-Pro.local>
2023-12-05 10:47:52 -0800

    first half model setup

--- server/models.py
+++ server/models.py
@@ -1,6 +1,59 @@
-from sqlalchemy_serializer import SerializerMixin
-from sqlalchemy.ext.associationproxy import association_proxy
+# server/models.py
 
-from config import db
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 
-# Models go here!
+from config import db, metadata
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String)
+
+    # Relationship mapping the user to related comments
+    comments = db.relationship(
+        'Comment', back_populates="user", cascade='all, delete-orphan')
+
+    # Relationship mapping user to related posts
+    posts = db.relationship(
+        'Post', uselist=False, back_populates='user', cascade='all, delete-orphan')
+
+    def __repr__(self):
+        return f'<User {self.id}, {self.name} />'
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String)
+
+    # Foreign key to store the user id
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    # Relationship mapping post to related user
+    user = db.relationship('User', back_populates='posts')
+    comments = db.relationship('Comment', back_populates='post')
+
+    def __repr__(self):
+        return f'<Post {self.id}, {self.description} />'
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+
+    # Foreign key to store the user id
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+
+    # Relationship mapping the review to related user
+    user = db.relationship('User', back_populates="comments")
+    post = db.relationship('Post', back_populates="comments")
+
+    def __repr__(self):
+        return f'<Review {self.id}, {self.comment} />'
--- server/seed.py
+++ server/seed.py
@@ -7,11 +7,89 @@ from random import randint, choice as rc
 from faker import Faker
 
 # Local imports
-from app import app
-from models import db
+from config import app, db
+# from app_setup import db
+from models import User
+from models import Post
+from models import Comment
+
+
+
+fake = Faker()
+
+def create_users():
+    users = []
+    for _ in range(10):
+        # pw_hash = flask_bcrypt.generate_password_hash("password").decode("utf-8")
+        c = User(
+            first_name=fake.first_name(),
+            # last_name=fake.last_name(),
+            # email=fake.email(),
+            # phone_number=fake.phone_number(),
+            # address=fake.address(),
+            # is_employee=fake.boolean(chance_of_getting_true=20),
+        )
+        # c.password = "password"
+        users.append(c)
+
+    return users
+
+
+
+def create_posts(users):
+    posts = []
+    for _ in range(20):
+        p = Post(
+            user_id=rc([user.id for user in users]),
+            description = fake.sentence()
+        )
+        posts.append(p)
+
+    return posts
+
+
+
+def create_comments(users, posts):
+    comments = []
+
+    for _ in range(40):
+        c = Comment(
+            user_id=rc([user.id for user in users]),
+            post_id=rc([post.id for post in posts]),
+            comment = fake.sentence()
+        )
+        comments.append(c)
+
+    return comments
+
+
 
 if __name__ == '__main__':
-    fake = Faker()
+
     with app.app_context():
-        print("Starting seed...")
-        # Seed code goes here!
+        print("Clearing db...")
+        # User.query.delete()
+        # Post.query.delete()
+        # Comment.query.delete()
+        
+        print("Creating tables...")
+        db.create_all()
+
+        print("Seeding users...")
+        users = create_users()
+        db.session.add_all(users)
+        db.session.commit()
+
+        print("Seeding posts...")
+        posts = create_posts(users)
+        db.session.add_all(posts)
+        db.session.commit()
+
+        print("Seeding comments...")
+        comments = create_comments(users, posts)
+        db.session.add_all(comments)
+        db.session.commit()
+
+        print("Seeding complete!!!")
+        
+print("Flask app and SQLAlchemy imported in seed.py.")
