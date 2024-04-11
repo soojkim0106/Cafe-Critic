bf09f17ba0db5afc1d1db8e2dfca675c7527e4bd
Soojungkim <soojung.kimm@gmail.com>
2024-04-11 10:11:22 -0700

    initial commit

--- server/schemas/cat_schema.py
+++ server/schemas/cat_schema.py
@@ -1,4 +1,5 @@
 from config import ma
+from marshmallow import validate, validates, ValidationError, fields, validate
 from models.cat import Cat
 
 class CatSchema(ma.SQLAlchemyAutoSchema):
@@ -6,3 +7,26 @@ class CatSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Cat
         load_instance = True
+        
+        users = fields.Nested(
+            "UserSchema",
+            only=("id", "username", "email"),
+            exclude=("cat",),
+            many=True,
+        )
+        
+        name = fields.String(required=True, validate=validate.Length(min=2,max=15))
+        age = fields.Integer(validate=validate.Range(min=1, max=20))
+        # gender = fields.String()
+        # breed = fields.String()
+        # temperament = fields.String()
+        image = fields.String(validate=validate.Regexp(
+            r".*\.(jpeg|png|jpg)", error="File URI must be in JPEG, JPG, or PNG format"
+            ),
+        )
+
+#!For ONE cat
+cat_schema = CatSchema()
+
+#! For MULTIPLE cats
+cats_schema = CatSchema(many=True)
--- server/schemas/user_schema.py
+++ server/schemas/user_schema.py
@@ -1,8 +1,32 @@
-from config import ma
+from config import ma, db
+import re
 from models.user import User
+from marshmallow import validate, validates, ValidationError, fields, validate, validates_schema
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
     
     class Meta:
         model = User
         load_instance = True
+        
+        cats = fields.Nested(
+            "CatSchema",
+            only=("id", "name"),
+            exclude=("user",),
+            many=True,
+        )
+        
+        username = fields.String(required=True, validate=validate.Length(min=2,max=20))
+        email = fields.String(required=True, validate=[validate.Email()])
+        _password_hash = fields.String(required=True, validate=validate.Length(min=5, max=10))
+        # interest = fields.String(required=False)
+        
+        @validates_schema
+        def validate_email(self, data, **kwargs):
+            email = data.get("email")
+            
+            if db.session.get(User, email).count():
+                raise ValidationError("Email {email} already exists.")
+        
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
